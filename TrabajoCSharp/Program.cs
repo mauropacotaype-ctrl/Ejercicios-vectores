468e11f9295d1e3aaeb8f70afab0f34fce2632f6
Mauro Pacotaype <mauropacotaype@gmail.com>
2026-05-27 22:06:43 -0500

    true

--- TrabajoCSharp/Program.cs
+++ TrabajoCSharp/Program.cs
@@ -1,11 +1,37 @@
-﻿class Program
+﻿using System;
+class Program
 {
     static void Main()
-    {   
-        Console.WriteLine("La suma de los cuadrados de los primeros 100 números enteros naturales es:\n ");
-        Console.WriteLine("La formula empleada es: n(n+1)(2n+1)/6 "  );
-        int a = 100;
-        int resultado = (a*(a+1)*(2*a+1))/6;
-        Console.WriteLine("El resultado es: " + resultado);   
+    {
+        Console.WriteLine("Operacion de suma y productos de dos fracciones");
+        Console.WriteLine("Fraccion 1:");
+        Console.WriteLine("Inserte un numero para el numerador:");
+        int A = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Inserte un numero para el denominador:");
+        int B = int.Parse(Console.ReadLine()!);
+
+        if (A < 0 || B <= 0)
+        {
+            Console.WriteLine("Error: Numerador debe ser mayor o igual a 0 y denominador debe ser mayor a 0.");
+            return;
+        }
+        Console.WriteLine("Fraccion 2:");
+        Console.WriteLine("Inserte un numero para el numerador:");
+        int C = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Inserte un numero para el denominador:");
+        int D = int.Parse(Console.ReadLine()!);
+        double Suma ;
+        double Producto ;
+    if (A >= 0 && B > 0 && C >= 0 && D > 0)
+        {
+            Suma = (double)((A * D) + (C * B)) / (B * D);
+            Producto = (double)(A * C) / (B * D);
+            Console.WriteLine("El resultado de la suma es: " + Suma);
+            Console.WriteLine("El resultado del producto es: " + Producto);
+        }
+        else
+        {
+            Console.WriteLine("Error: Numeradores deben ser mayores o iguales a 0 y denominadores deben ser mayores a 0."); 
+        }
     }
 }
--- TrabajoCSharp/obj/Debug/net10.0/TrabajoCSharp.AssemblyInfo.cs
+++ TrabajoCSharp/obj/Debug/net10.0/TrabajoCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabajoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e1a70d714ad48b249380db8367ba4b166e8a32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d57f1719dc93575a8c0702ba5f672cf77bdfe21a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabajoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabajoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
