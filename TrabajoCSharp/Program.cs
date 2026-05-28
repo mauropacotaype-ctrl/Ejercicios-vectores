bbdbfedb4e3895bcdda1ff442314eba48c8dfda0
Mauro Pacotaype <mauropacotaype@gmail.com>
2026-05-27 23:03:15 -0500

    true

--- TrabajoCSharp/Program.cs
+++ TrabajoCSharp/Program.cs
@@ -1,37 +1,30 @@
-﻿using System;
-class Program
+﻿using System;   
+class Program 
 {
     static void Main()
-    {
-        Console.WriteLine("Operacion de suma y productos de dos fracciones");
-        Console.WriteLine("Fraccion 1:");
-        Console.WriteLine("Inserte un numero para el numerador:");
-        int A = int.Parse(Console.ReadLine()!);
-        Console.WriteLine("Inserte un numero para el denominador:");
-        int B = int.Parse(Console.ReadLine()!);
+    {   
+            Console.Write( "introduce un numero entero positivo para calcular su factorial: " );
+            int numero = int.Parse( Console.ReadLine()! );
+            int i =1;
+            int j =1;
+            int factorial = 1;
+            if ( numero < 0)
+                { 
+                    Console.WriteLine( " valor no calido ( debe ser positivo y entero )" );
+                return;
+                }
 
-        if (A < 0 || B <= 0)
-        {
-            Console.WriteLine("Error: Numerador debe ser mayor o igual a 0 y denominador debe ser mayor a 0.");
-            return;
-        }
-        Console.WriteLine("Fraccion 2:");
-        Console.WriteLine("Inserte un numero para el numerador:");
-        int C = int.Parse(Console.ReadLine()!);
-        Console.WriteLine("Inserte un numero para el denominador:");
-        int D = int.Parse(Console.ReadLine()!);
-        double Suma ;
-        double Producto ;
-    if (A >= 0 && B > 0 && C >= 0 && D > 0)
-        {
-            Suma = (double)((A * D) + (C * B)) / (B * D);
-            Producto = (double)(A * C) / (B * D);
-            Console.WriteLine("El resultado de la suma es: " + Suma);
-            Console.WriteLine("El resultado del producto es: " + Producto);
-        }
-        else
-        {
-            Console.WriteLine("Error: Numeradores deben ser mayores o iguales a 0 y denominadores deben ser mayores a 0."); 
-        }
+            else{
+            
+                for ( i = 1 ; i <= numero ; i++ )
+                    {
+                        factorial = 1;
+                        for ( j= 1 ;j<= i ; j++)
+                            { 
+                                factorial = factorial*j;
+                            }
+                            Console.WriteLine( "el factorial de " + i +" es: " + factorial );
+                    }
+                }
     }
 }
