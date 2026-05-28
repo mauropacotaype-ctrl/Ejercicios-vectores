486976a9d0a5ff06d0d332b661d17d8b931039bb
Mauro Pacotaype <mauropacotaype@gmail.com>
2026-05-28 10:57:45 -0500

    true

---
+++ remoto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Lista de puntos (x, y)
+        List<(double x, double y)> puntos = new List<(double, double)>
+        {
+            (5,4),(6,4),(7,4),(-5,4),(-7,4),(0,0),(3,3)
+        };
+        int contador = 0;
+        foreach (var p in puntos)
+        {
+            double x = p.x;
+            double y = p.y;
+
+            // Circunferencia c1
+            bool dentroC1 = Math.Pow(x - 5, 2) + Math.Pow(y - 4, 2) <= Math.Pow(2, 2);
+
+            // Circunferencia c2
+            bool dentroC2 = Math.Pow(x + 5, 2) + Math.Pow(y - 4, 2) <= Math.Pow(3, 2);
+
+            if (dentroC1 || dentroC2)
+            {
+                contador++;
+                Console.WriteLine($"Punto ({x},{y}) está dentro.");
+            }
+        }
+        Console.WriteLine($"\nCantidad total de puntos dentro de las circunferencias: {contador}");
+    }
+}
