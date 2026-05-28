76e1a70d714ad48b249380db8367ba4b166e8a32
Mauro Pacotaype <mauropacotaype@gmail.com>
2026-05-27 20:25:43 -0500

    true

--- TrabajoCSharp/Program.cs
+++ TrabajoCSharp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿ 
+            Console.Write( "introduce un numero entero positivo para calcular su factorial: " );
+            int numero = int.Parse( Console.ReadLine() );
+
+            if ( numero < 0)
+            { 
+                Console.WriteLine( " valor no calido ( debe ser positivo y entero )" );
+                return;
+            }
+
+            else{
+            int factorial = 1;
+            for ( int i = 1 ; i <= numero ; i++ )
+                        {
+                            factorial = factorial *i;
+                        }
+            Console.WriteLine( "el factorial de " + numero + " es: " + factorial );         
+                    }
